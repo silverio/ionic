f02a0bfa322be60d3e896bd0175a1a2f63c76ff9
Adam Bradley <adambradley25@gmail.com>
2018-05-11 23:37:23 -0500

    select value

--- angular/test/testapp/src/app/basic-inputs-page/basic-inputs-page.component.ts
+++ angular/test/testapp/src/app/basic-inputs-page/basic-inputs-page.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-basic-inputs-page',
   templateUrl: './basic-inputs-page.component.html',
   encapsulation: ViewEncapsulation.None
 })
-export class BasicInputsPageComponent implements OnInit {
+export class BasicInputsPageComponent {
   disableInputs = false;
 
   datetimeValue = '2017-11-18T14:17:45-06:00';
   textareaValue = 'This is the Textarea Input';
   textValue = 'This is the Text Input';
   numericValue = 1138;
+  selectValue = 'pineapple';
 
   checkboxValue = true;
   toggleValue = false;
@@ -19,10 +20,6 @@ export class BasicInputsPageComponent implements OnInit {
   rangeValue = 15;
 
   searchValue: string;
-
-  constructor() {}
-
-  ngOnInit() {}
 
   typeOf(v: any): string {
     return typeof v;
